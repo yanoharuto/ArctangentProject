7e870efd86cd052c100c4c70b06b30599eb9e56e
yanoharuto <8bitharuto@gmail.com>
2022-08-17 05:37:09 +0900

    ハンマーとりあえず実装。あと破壊される前にどのオブジェクトが破壊されるか報告できるようにした。

---
+++ OmaturiGame/Assets/Gimmick/Hammer/Script/Hammer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 設置すると他のオブジェクトを破壊するよ
+/// </summary>
+public class Hammer : GimmickBase
+{
+    protected override void Run()
+    {
+        Destroy(this.gameObject);
+    }
+    protected override void SetUp()
+    { 
+    }
+    protected override void Standby()
+    {
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (m_GimmickState == GimmickState.Hammer)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
