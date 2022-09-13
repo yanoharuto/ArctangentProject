6642e03655c2e42a250fa086e8b1d5f5da035374
fca <fca>
2022-09-13 11:41:56 +0900

    コインとゴールのスコアを加算できるようにしました

---
+++ OmaturiGame/Assets/Yonemori/Assets/goalController.cs
@@ -0,0 +1,24 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class go : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+//    void OnTriggerEnter2D(Collider2D collision)               //衝突したオブジェクトのタグをcollisionに代入
+//    {
+//        Debug.Log(collision.gameObject.name);              //衝突したオブジェクトの名前を表示
+
+        
+//    }
+//}
