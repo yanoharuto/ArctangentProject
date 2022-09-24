f26135278122ebac2e2918f164cc52e2096700c2
yanoharuto <8bitharuto@gmail.com>
2022-09-24 14:50:34 +0900

    Titleちょっと変更

--- OmaturiGame/Assets/MainScene/Title/Go2GameScene.cs
+++ OmaturiGame/Assets/MainScene/Title/Go2GameScene.cs
@@ -6,9 +6,15 @@ using UnityEngine.SceneManagement;
 public class Go2GameScene : MonoBehaviour
 {
     [SerializeField] string m_NextScene;
-
+    [SerializeField] GameObject m_TutorialObj;
+    [SerializeField] AudioSource m_Audio;
     public void OnClickGo2GameSceneButton()
     {
+        m_Audio.Play();
         SceneManager.LoadScene(m_NextScene);
+    }
+    public void OnPlayTutorial()
+    {
+        Instantiate(m_TutorialObj);
     }
 }
